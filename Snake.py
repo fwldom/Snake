f704b3a8b7868346d0c7eaf0829bbce845e67fe5
Fwldom <111840687+fwldom@users.noreply.github.com>
2023-06-10 22:11:41 +0330

    F

--- Snake.py
+++ Snake.py
@@ -3,3 +3,15 @@ try:
 except:
     import os
     os.system("pip install pygame")
+import pygame
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.display.set_caption('Snake Game by Faradars')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)   #prints out all the actions that take place on the screen
+ 
+pygame.quit()
+quit()
